41223a69ad6e969ef4f61b3bbe216c9ce48cf70c
kilbergr <rekilberg@gmail.com>
2015-06-03 22:54:44 -0700

    combo calculator

--- app.js
+++ app.js
@@ -5,11 +5,55 @@ var app = express();
 app.set("view engine", "ejs");
 
 
-app.get("/add/:num1/:sign/:num2", function(req, res){
-	var ans = parseInt(req.params.num1) + req.params.sign + parseInt(req.params.num2);
+//with decimals handled
+app.get("/:sign/:num1/:num2", function(req, res){
+	var signs = {
+		'add': function(a, b){return a + b},
+		'subtract': function(a, b){return a-b},
+		'multiply': function(a, b){return a*b},
+		'divide': function(a,b){return a/b}
+	};
+
+	if (req.params.sign==="add"){
+		sign = signs.add;}
+	else if (req.params.sign==="subtract"){
+		sign = signs.subtract;
+	}
+	else if (req.params.sign==="multiply"){
+		sign = signs.multiply;
+	}
+	else {
+		sign = signs.divide;
+	}
+ 	var ans = sign(parseFloat(req.params.num1), parseFloat(req.params.num2));
 	res.render("mathOps", {solution: ans})
 })
 
+/*separately
+
+app.get("/add/:num1/:num2", function(req, res){
+	var ans = parseFloat(req.params.num1) + parseFloat(req.params.num2);
+	res.render("mathOps", {solution: ans})
+})
+
+
+app.get("/subtract/:num1/:num2", function(req, res){
+	var ans = parseFloat(req.params.num1) - parseFloat(req.params.num2);
+	res.render("mathOps", {solution: ans})
+})
+
+
+app.get("/multiply/:num1/:num2", function(req, res){
+	var ans = parseFloat(req.params.num1) * parseFloat(req.params.num2);
+	res.render("mathOps", {solution: ans})
+})
+
+
+app.get("/divide/:num1/:num2", function(req, res){
+	var ans = parseFloat(req.params.num1)/parseFloat(req.params.num2);
+	res.render("mathOps", {solution: ans})
+})
+*/
 //localhost:3000
 app.listen(3000, function(){
 })
